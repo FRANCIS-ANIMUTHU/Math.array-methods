e86b14bb69030bfd1c8c848ad3939b686f7316ba
FRANCIS-ANIMUTHU <104548375+FRANCIS-ANIMUTHU@users.noreply.github.com>
2022-08-11 23:13:47 +0530

    Add files via upload

---
+++ numbers/experiment.js
@@ -0,0 +1,96 @@
+//abs
+x=Math.abs(-78);
+console.log('abs=',x);
+
+//ceil
+a=Math.ceil(67.4);
+console.log('ceil=',a);
+
+//floor
+a=Math.floor(98.6);
+console.log('floor=',a);
+
+//max
+a=Math.max(67,34,99);
+console.log('maximum=',a);
+
+//min
+a=Math.min(45,67,32,87);
+console.log('minimum=',a);
+
+//pow
+a=Math.pow(45,3);
+console.log('power=',a);
+
+//random
+a=Math.random();
+console.log('random=',a+4);
+
+//round
+a=Math.round(67.5);
+console.log('round=',a);
+
+//sqrt
+a=Math.sqrt(144);
+console.log('square root=',a);
+
+
+
+//                    < String >
+//charAt
+var s="francis";
+var y=s.charAt(2);
+console.log('charAt=',y);
+
+//uppercase
+var s="francis";
+var y=s.toUpperCase();
+console.log('uppercase=',y);
+
+//concat
+var a="Francis";
+var b=" Maass";
+var c=a.concat(b);
+console.log('concat=',c);
+
+//includes
+let pet=['dog','cat','lion'];
+console.log(pet.includes('parrot'));
+
+//indexOf
+let friends=['Gnanasekar','Arun','Sudhakar','Francis','Venkat','Deepak'];
+console.log(friends.indexOf('Besky'));
+
+//lastIndexOf
+const frd=['Gnanasekar','Arun','Sudhakar','Francis','Venkat','Deepak'];
+console.log(frd.lastIndexOf('Arun'));
+
+//Replace
+let p='my name is Francis';
+console.log(p.replace('Francis','Surya'));
+
+//raplaceAll
+let Q='my name is Maass.my nick name is Francis';
+console.log(Q.replaceAll('Maass','Francis'));
+
+//slice
+let z='i love cricket.my favorite player is yuvi';
+console.log(z.slice(7,15));
+
+//startswith
+let n='I am Surya fan';
+console.log(n.startsWith('I am Surya'));
+
+//substring
+let o='paragraph';
+console.log(o.substring(4));
+
+
+
+
+
+
+
+
+
+
